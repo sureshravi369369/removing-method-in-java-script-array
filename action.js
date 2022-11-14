7b65504cc5030d692d607d26e5ca556c018346cc
Suresh <114983422+sureshravi369369@users.noreply.github.com>
2022-11-14 17:30:23 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,19 @@
+// removing elements:
+
+// pop method:
+const number=[1,2,3,4,5]
+number.pop()
+
+console.log(number);
+
+// shift method:
+const items=["apple","orange","lemon"]
+items.shift()
+
+console.log(items);
+
+// splice method:
+const twoWhelers=["hero","ktm","duke"]
+twoWhelers.splice(1,1,"tvs")
+
+console.log(twoWhelers);
